5cc583d35b1d05e7e1e85d05150e1b180b2e306c
walkieworld <Hao.Zhang@du.edu>
2015-06-25 23:44:52 -0600

    Update first screen style

--- js/main.js
+++ js/main.js
@@ -2,10 +2,44 @@
  * Created by hao on 6/21/15.
  */
 $("main").stellar();
+
 $(function(){
     $.stellar({
         horizontalScrolling: false,
         verticalOffset: 40
     });
 });
+
 $.backstretch("img/ITStuff_web.jpg");
+
+(function () {
+    'use strict'
+    var scrollAnimationCollection = document.querySelectorAll("[data-scroll-animation='90%']");
+    for(var i = 0; i < scrollAnimationCollection.length; i++){
+        var waypoint = new Waypoint({
+            element: scrollAnimationCollection.item(i),
+            handler: function(direction) {
+                this.element.classList.add('in');
+            },
+            offset: '90%'
+        });
+    }
+
+    if (window.scrollY === 0){
+        document.querySelector('body>nav').classList.remove('scrolled');
+    }
+    else{
+        document.querySelector('body>nav').classList.add('scrolled');
+    }
+
+    $(window).scroll(function(){
+        if (window.scrollY === 0){
+            document.querySelector('body>nav').classList.remove('scrolled');
+        }
+        else{
+            document.querySelector('body>nav').classList.add('scrolled');
+        }
+    });
+})();
+
+
