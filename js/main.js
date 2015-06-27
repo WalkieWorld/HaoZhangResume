b0977a2bbf8141d4148a3f704c8a876e550a7118
walkieworld <Hao.Zhang@du.edu>
2015-06-27 00:31:00 -0600

    Add parallax bg and pre-page loader.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,64 @@
 /**
  * Created by hao on 6/21/15.
  */
-$("main").stellar();
 
-$(function(){
-    $.stellar({
-        horizontalScrolling: false,
-        verticalOffset: 40
-    });
+$(window).load(function(){
+    $('#page-loader').fadeOut(500);
 });
 
-$.backstretch("img/ITStuff_web.jpg");
+$(function(){
 
-(function () {
-    'use strict'
-    var scrollAnimationCollection = document.querySelectorAll("[data-scroll-animation='90%']");
-    for(var i = 0; i < scrollAnimationCollection.length; i++){
-        var waypoint = new Waypoint({
-            element: scrollAnimationCollection.item(i),
-            handler: function(direction) {
-                this.element.classList.add('in');
-            },
-            offset: '90%'
-        });
-    }
+    $('.jumbotron').height($(window).height());
 
-    if (window.scrollY === 0){
-        document.querySelector('body>nav').classList.remove('scrolled');
-    }
-    else{
-        document.querySelector('body>nav').classList.add('scrolled');
-    }
+    $('.header-box').css({'marginTop':$(window).height()*0.4});
 
-    $(window).scroll(function(){
+    $(window).resize(function(){
+        $('.jumbotron').height($(window).height());
+        $('.header-box').css({'marginTop':$(window).height()*0.4});
+    });
+
+    $.backstretch("img/ITStuff_web.jpg");
+
+    document.querySelector('.backstretch').setAttribute('data-stellar-ratio', '0.5');
+
+    setTimeout(function () {
+        'use strict'
+        var scrollAnimationCollection = document.querySelectorAll("[data-scroll-animation='90%']");
+        for(var i = 0; i < scrollAnimationCollection.length; i++){
+            var waypoint = new Waypoint({
+                element: scrollAnimationCollection.item(i),
+                handler: function(direction) {
+                    this.element.classList.add('in');
+                },
+                offset: '90%'
+            });
+        }
+
         if (window.scrollY === 0){
             document.querySelector('body>nav').classList.remove('scrolled');
         }
         else{
             document.querySelector('body>nav').classList.add('scrolled');
         }
+
+        $(window).scroll(function(){
+            if (window.scrollY === 0){
+                document.querySelector('body>nav').classList.remove('scrolled');
+            }
+            else{
+                document.querySelector('body>nav').classList.add('scrolled');
+            }
+        });
+    }, 600);
+
+    $(".backstretch").stellar();
+
+    $("main").stellar();
+
+    $(function(){
+        $.stellar({
+            horizontalScrolling: false,
+            verticalOffset: 40
+        });
     });
-})();
-
-
+});
