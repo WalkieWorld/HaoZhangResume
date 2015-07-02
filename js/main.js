b75d3f020257ee6f36fd32e5ad037e70f6859fc5
walkieworld <Hao.Zhang@du.edu>
2015-07-01 23:59:58 -0600

    Optimized waypoints script

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@
  * Created by hao on 6/21/15.
  */
 
+function scrollSpyRefresh(){
+    setTimeout(function(){
+        $('body').scrollspy('refresh');
+    },1000);
+}
+
 $(window).load(function(){
     $('#page-loader').fadeOut(500);
+    scrollSpyRefresh();
 });
 
 $(".backstretch").stellar();
@@ -17,6 +24,7 @@ $(function(){
     $(window).resize(function(){
         $('.jumbotron').height($(window).height());
         $('.header-box').css({'marginTop':$(window).height()*0.4});
+        scrollSpyRefresh();
     });
 
     $.backstretch("img/ITStuff_web.jpg");
@@ -25,16 +33,58 @@ $(function(){
 
     setTimeout(function () {
         'use strict'
-        var scrollAnimationCollection = document.querySelectorAll("[data-scroll-animation='90%']");
-        for(var i = 0; i < scrollAnimationCollection.length; i++){
+        new Promise(function(resolve, reject){
+            var scrollAnimationCollection90 = document.querySelectorAll("[data-scroll-animation='90%']");
+            resolve(scrollAnimationCollection90);
+        }).then(function(collection){
+                for(var i = 0; i < collection.length; i++){
+                    var waypoint = new Waypoint({
+                        element: collection.item(i),
+                        handler: function(direction) {
+                            this.element.classList.add('in');
+                        },
+                        offset: '90%'
+                    });
+                }
+            }).catch(function(err){
+                console.log("Failed!", err);
+            });
+        new Promise(function(resolve, reject){
+            var scrollAnimationCollection95 = document.querySelectorAll("[data-scroll-animation='95%']");
+            resolve(scrollAnimationCollection95);
+        }).then(function(collection){
+                for(var i = 0; i < collection.length; i++){
+                    var waypoint = new Waypoint({
+                        element: collection.item(i),
+                        handler: function(direction) {
+                            this.element.classList.add('in');
+                        },
+                        offset: '95%'
+                    });
+                }
+            }).catch(function(err){
+                console.log("Failed!", err);
+            });;
+/*        var scrollAnimationCollection90 = document.querySelectorAll("[data-scroll-animation='90%']");
+        var scrollAnimationCollection95 = document.querySelectorAll("[data-scroll-animation='95%']");
+        for(var i = 0; i < scrollAnimationCollection90.length; i++){
             var waypoint = new Waypoint({
-                element: scrollAnimationCollection.item(i),
+                element: scrollAnimationCollection90.item(i),
                 handler: function(direction) {
                     this.element.classList.add('in');
                 },
                 offset: '90%'
             });
         }
+        for(var i = 0; i < scrollAnimationCollection95.length; i++){
+            var waypoint = new Waypoint({
+                element: scrollAnimationCollection95.item(i),
+                handler: function(direction) {
+                    this.element.classList.add('in');
+                },
+                offset: '95%'
+            });
+        }*/
 
         if (window.scrollY === 0){
             document.querySelector('body>nav').classList.remove('scrolled');
@@ -61,4 +111,10 @@ $(function(){
             verticalOffset: 40
         });
     });
+
+    $('a[data-scroll-smooth]').click(function(e){
+        $('html,body').scrollTo(this.hash, this.hash, {offset:{top: -100, left: 0}});
+        e.preventDefault();
+    });
+
 });
